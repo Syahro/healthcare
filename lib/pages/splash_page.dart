3bcbc2af792158a9cf228dd51b1aec7c1cc182fc
Arief Syahroni <riefsyah31@gmail.com>
2021-08-24 11:04:08 +0700

    done modified splashPage and add some assets or support

---
+++ lib/pages/splash_page.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'package:healthcare/theme.dart';
+
+class SplashPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: defaultMargin,
+          ),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Welcome to',
+                  style: headingFive.copyWith(
+                    color: primaryOne,
+                  ),
+                ),
+                SizedBox(
+                  height: 4,
+                ),
+                Text(
+                  'Self Care',
+                  style: headingThree.copyWith(
+                    color: primaryOne,
+                  ),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                Image.asset(
+                  'assets/illustration.png',
+                  width: double.infinity,
+                  height: 320,
+                ),
+                SizedBox(
+                  height: 46,
+                ),
+                Container(
+                  height: 42,
+                  width: double.infinity,
+                  child: FlatButton(
+                    onPressed: () {},
+                    child: Text(
+                      'Sign Up',
+                      style: buttonTwo.copyWith(
+                        color: whiteColor,
+                      ),
+                    ),
+                    color: primaryOne,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                Container(
+                  height: 42,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8),
+                    border: Border.all(
+                      color: primaryOne,
+                    ),
+                  ),
+                  child: FlatButton(
+                    onPressed: () {},
+                    child: Text(
+                      'Login',
+                      style: buttonTwo.copyWith(
+                        color: primaryOne,
+                      ),
+                    ),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/theme.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+final double defaultMargin = 24.0;
+
+Color whiteColor = Color(0XFFFFFFFF);
+Color primaryOne = Color(0XFF432C81);
+Color primaryTwo = Color(0XFF56428F);
+Color primaryThree = Color(0XFF7B6BA8);
+Color primaryFour = Color(0XFFA095C1);
+Color primaryFive = Color(0XFFC6BFD9);
+Color primarySix = Color(0XFFEDECF4);
+Color yellowColor = Color(0XFFFEC62F);
+Color redColor = Color(0XFFEB5858);
+Color blueColor = Color(0XFF29C9FB);
+
+TextStyle headingOne = GoogleFonts.raleway(
+  fontSize: 56,
+  fontWeight: FontWeight.w700,
+  letterSpacing: -1,
+);
+
+TextStyle headingTwo = GoogleFonts.raleway(
+  fontSize: 40,
+  fontWeight: FontWeight.w700,
+  letterSpacing: -1,
+);
+
+TextStyle headingThree = GoogleFonts.raleway(
+  fontSize: 32,
+  fontWeight: FontWeight.w700,
+  letterSpacing: -1,
+);
+
+TextStyle headingFour = GoogleFonts.raleway(
+  fontSize: 24,
+  fontWeight: FontWeight.w700,
+  letterSpacing: -1,
+);
+
+TextStyle headingFive = GoogleFonts.raleway(
+  fontSize: 20,
+  fontWeight: FontWeight.w700,
+  letterSpacing: -1,
+);
+
+TextStyle paragraphOne = GoogleFonts.raleway(
+  fontSize: 16,
+  fontWeight: FontWeight.w500,
+);
+
+TextStyle paragraphTwo = GoogleFonts.raleway(
+  fontSize: 14,
+  fontWeight: FontWeight.w500,
+);
+
+TextStyle paragraphThree = GoogleFonts.raleway(
+  fontSize: 12,
+  fontWeight: FontWeight.w500,
+);
+
+TextStyle buttonOne = GoogleFonts.raleway(
+  fontSize: 18,
+  fontWeight: FontWeight.w600,
+  letterSpacing: 0.5,
+);
+
+TextStyle buttonTwo = GoogleFonts.raleway(
+  fontSize: 16,
+  fontWeight: FontWeight.w600,
+);
+
+TextStyle buttonThree = GoogleFonts.raleway(
+  fontSize: 14,
+  fontWeight: FontWeight.w600,
+);
